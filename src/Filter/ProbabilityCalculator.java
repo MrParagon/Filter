8f9b8d30d8fc1ab93e1397a081612aa1c7307b4b
Daniel Miller <daniel.miller21@gmail.com>
2014-10-26 23:37:06 -0400

    Added output to file

--- src/Filter/ProbabilityCalculator.java
+++ src/Filter/ProbabilityCalculator.java
@@ -1,63 +1,81 @@
 package Filter;
 import java.util.*;
+import java.io.*;
 
 public class ProbabilityCalculator {
 
 	public static void calcSmoothedProbability(ArrayList<String> hamDict, ArrayList<String> spamDict)
 	{
-		ArrayList<String> uniqueWords = new ArrayList<String>();
-		int totalHamCount = hamDict.size();
-		int totalSpamCount = spamDict.size();
-		int hamCount = 0;
-		int spamCount = 0;
-		float hamProb = 0;
-		float spamProb = 0;
-		
-		for (String str : hamDict)
+		try
 		{
-			if (!uniqueWords.contains(str))
-			{
-				uniqueWords.add(str);
-			}
-		}
-		
-		for (String str : spamDict)
-		{
-			if (!uniqueWords.contains(str))
-			{
-				uniqueWords.add(str);
-			}
-		}
-		
-		int vocabSize = uniqueWords.size();
-		
-		for (String word : uniqueWords)
-		{
+
+			File file = new File("Model.txt");
+			BufferedWriter output = new BufferedWriter(new FileWriter(file));
+
+			ArrayList<String> uniqueWords = new ArrayList<String>();
+			int totalHamCount = hamDict.size();
+			int totalSpamCount = spamDict.size();
+			int hamCount = 0;
+			int spamCount = 0;
+			float hamProb = 0;
+			float spamProb = 0;
+
 			for (String str : hamDict)
 			{
-				if (word.equals(str))
+				if (!uniqueWords.contains(str))
 				{
-					hamCount++;
+					uniqueWords.add(str);
 				}
 			}
-			
+
 			for (String str : spamDict)
 			{
-				if (word.equals(str))
+				if (!uniqueWords.contains(str))
 				{
-					spamCount++;
+					uniqueWords.add(str);
 				}
 			}
+
+			int vocabSize = uniqueWords.size();
+
+			for (String word : uniqueWords)
+			{
+				for (String str : hamDict)
+				{
+					if (word.equals(str))
+					{
+						hamCount++;
+					}
+				}
+
+				for (String str : spamDict)
+				{
+					if (word.equals(str))
+					{
+						spamCount++;
+					}
+				}
+
+				hamProb = probability(hamCount, totalHamCount, vocabSize);
+				spamProb = probability(spamCount, totalSpamCount, vocabSize);
+				
+				String out = word + "   " + hamCount + "   " + hamProb + "   " + spamCount + "   " + spamProb;
+				System.out.println(out);
+				output.write(out);
+				output.newLine();
+				hamCount = 0;
+				spamCount = 0;
+			}
 			
-			hamProb = probability(hamCount, totalHamCount, vocabSize);
-			spamProb = probability(spamCount, totalSpamCount, vocabSize);
-			System.out.println(word + "   " + hamCount + "   " + hamProb + "   " + spamCount + "   " + spamProb);
-		
-			hamCount = 0;
-			spamCount = 0;
+			output.close();
+
+		}
+		catch (Exception e)
+		{
+
 		}
 	}
-	
+
 	public static float probability(int count, int totalCount, int vocabSize)
 	{
 		float result = 0;
